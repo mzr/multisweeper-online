6e7d5b4b25d830cbfb3a686021586cd7e7c984ec
dingorth <281141@uwr.edu.pl>
2017-01-14 21:47:20 +0100

    working

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,10 +11,5 @@ export class AppComponent {
     constructor() {}
 }
 
-// this use to be a template
-/*
-<h1>My First {{name}} app</h1>
-<router-outlet></router-outlet>
 
-<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>*/
 
