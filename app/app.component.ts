33c1a5ddda13ba50d654c7248e8abb2c04d0c768
dingorth <281141@uwr.edu.pl>
2017-01-14 21:34:07 +0100

    a

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    styles: [`h1 {
-	color: white;
-	background: darkgray;
-	padding: 20px;
-}
-`],
-    template: `
-<h1>My First {{name}} app</h1>
-<router-outlet></router-outlet>
-
-<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>`,
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
     name: string = "Angular 2 on Express";
 
     constructor() {}
 }
+
+// this use to be a template
+/*
+<h1>My First {{name}} app</h1>
+<router-outlet></router-outlet>
+
+<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>*/
+
